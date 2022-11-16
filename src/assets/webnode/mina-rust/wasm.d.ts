0d684a479bddd0412511ac3f38c6f60b6cba332d
Jolte Teofil <jolteteofil@yahoo.com>
2022-11-16 17:13:03 +0200

    New wasm

---
+++ src/assets/webnode/mina-rust/wasm.d.ts
@@ -0,0 +1,182 @@
+/* tslint:disable */
+/* eslint-disable */
+/**
+* @returns {Promise<JsHandle>}
+*/
+export function start(): Promise<JsHandle>;
+/**
+*/
+export class Crypto {
+  free(): void;
+/**
+* @returns {Uint8Array}
+*/
+  pub_key_as_protobuf(): Uint8Array;
+/**
+* @returns {string}
+*/
+  peer_id_as_b58(): string;
+/**
+* @param {Uint8Array} msg
+* @returns {Uint8Array}
+*/
+  sign(msg: Uint8Array): Uint8Array;
+/**
+* @param {Uint8Array} pub_key_as_protobuf
+* @returns {string}
+*/
+  pub_key_as_protobuf_to_peer_id_as_b58(pub_key_as_protobuf: Uint8Array): string;
+/**
+* @param {Uint8Array} pub_key_as_protobuf
+* @param {Uint8Array} msg
+* @param {Uint8Array} sig
+*/
+  assert_signature(pub_key_as_protobuf: Uint8Array, msg: Uint8Array, sig: Uint8Array): void;
+/**
+* @param {Uint8Array} pub_key_as_protobuf
+* @param {string} expected_peer_id
+*/
+  pub_key_as_protobuf_to_peer_id(pub_key_as_protobuf: Uint8Array, expected_peer_id: string): void;
+}
+/**
+*/
+export class InMemLog {
+  free(): void;
+/**
+* @param {string} key
+* @returns {string | undefined}
+*/
+  get(key: string): string | undefined;
+/**
+* @returns {number}
+*/
+  id(): number;
+/**
+* @returns {string}
+*/
+  as_json(): string;
+}
+/**
+*/
+export class JsHandle {
+  free(): void;
+/**
+* @param {number | undefined} cursor
+* @param {number | undefined} limit
+* @returns {Promise<any>}
+*/
+  logs_range(cursor?: number, limit?: number): Promise<any>;
+/**
+* @returns {ManualConnector}
+*/
+  manual_connector(): ManualConnector;
+/**
+* @param {string} id
+*/
+  is_peer_id_valid(id: string): void;
+/**
+* @returns {Promise<any>}
+*/
+  global_state_get(): Promise<any>;
+/**
+* @param {string} addr
+* @returns {Promise<string>}
+*/
+  peer_connect(addr: string): Promise<string>;
+/**
+* @param {string} topic
+* @param {any} msg
+* @returns {Promise<void>}
+*/
+  pubsub_publish(topic: string, msg: any): Promise<void>;
+/**
+* @returns {any}
+*/
+  generate_account_keys(): any;
+/**
+* @param {any} data
+* @returns {Promise<any>}
+*/
+  payment_sign_and_inject(data: any): Promise<any>;
+}
+/**
+*/
+export class ManualConnector {
+  free(): void;
+/**
+* @param {string} peer_id
+* @returns {Promise<Promise<any>>}
+*/
+  dial(peer_id: string): Promise<Promise<any>>;
+/**
+* @returns {any}
+*/
+  listen(): any;
+}
+
+export type InitInput = RequestInfo | URL | Response | BufferSource | WebAssembly.Module;
+
+export interface InitOutput {
+  readonly memory: WebAssembly.Memory;
+  readonly __wbg_inmemlog_free: (a: number) => void;
+  readonly inmemlog_get: (a: number, b: number, c: number, d: number) => void;
+  readonly inmemlog_id: (a: number) => number;
+  readonly inmemlog_as_json: (a: number, b: number) => void;
+  readonly __wbg_manualconnector_free: (a: number) => void;
+  readonly manualconnector_dial: (a: number, b: number, c: number) => number;
+  readonly manualconnector_listen: (a: number) => number;
+  readonly start: () => number;
+  readonly __wbg_jshandle_free: (a: number) => void;
+  readonly jshandle_logs_range: (a: number, b: number, c: number, d: number, e: number) => number;
+  readonly jshandle_manual_connector: (a: number) => number;
+  readonly jshandle_is_peer_id_valid: (a: number, b: number, c: number, d: number) => void;
+  readonly jshandle_global_state_get: (a: number) => number;
+  readonly jshandle_peer_connect: (a: number, b: number, c: number) => number;
+  readonly jshandle_pubsub_publish: (a: number, b: number, c: number, d: number) => number;
+  readonly jshandle_generate_account_keys: (a: number) => number;
+  readonly jshandle_payment_sign_and_inject: (a: number, b: number) => number;
+  readonly __wbg_crypto_free: (a: number) => void;
+  readonly crypto_pub_key_as_protobuf: (a: number, b: number) => void;
+  readonly crypto_peer_id_as_b58: (a: number, b: number) => void;
+  readonly crypto_sign: (a: number, b: number, c: number, d: number) => void;
+  readonly crypto_pub_key_as_protobuf_to_peer_id_as_b58: (a: number, b: number, c: number, d: number) => void;
+  readonly crypto_assert_signature: (a: number, b: number, c: number, d: number, e: number, f: number, g: number, h: number) => void;
+  readonly crypto_pub_key_as_protobuf_to_peer_id: (a: number, b: number, c: number, d: number, e: number, f: number) => void;
+  readonly __wbindgen_malloc: (a: number) => number;
+  readonly __wbindgen_realloc: (a: number, b: number, c: number) => number;
+  readonly __wbindgen_export_2: WebAssembly.Table;
+  readonly _dyn_core__ops__function__FnMut__A____Output___R_as_wasm_bindgen__closure__WasmClosure___describe__invoke__hf0d3ac44e8ea9df5: (a: number, b: number, c: number) => void;
+  readonly _dyn_core__ops__function__FnMut_____Output___R_as_wasm_bindgen__closure__WasmClosure___describe__invoke__h724fb25c7b600c05: (a: number, b: number) => void;
+  readonly _dyn_core__ops__function__FnMut_____Output___R_as_wasm_bindgen__closure__WasmClosure___describe__invoke__h5e2c95f786bffb19: (a: number, b: number) => void;
+  readonly __wbindgen_add_to_stack_pointer: (a: number) => number;
+  readonly __wbindgen_free: (a: number, b: number) => void;
+  readonly __wbindgen_exn_store: (a: number) => void;
+  readonly wasm_bindgen__convert__closures__invoke2_mut__h419d610678e97774: (a: number, b: number, c: number, d: number) => void;
+}
+
+export type SyncInitInput = BufferSource | WebAssembly.Module;
+/**
+* Instantiates the given `module`, which can either be bytes or
+* a precompiled `WebAssembly.Module`.
+*
+* @param {SyncInitInput} module
+*
+* @returns {InitOutput}
+*/
+export function initSync(module: SyncInitInput): InitOutput;
+
+/**
+* If `module_or_path` is {RequestInfo} or {URL}, makes a request and
+* for everything else, calls `WebAssembly.instantiate` directly.
+*
+* @param {InitInput | Promise<InitInput>} module_or_path
+*
+* @returns {Promise<InitOutput>}
+*/
+export default function init (module_or_path?: InitInput | Promise<InitInput>): Promise<InitOutput>;
+
+
+
+export type WasmLoadProgressEvent = Pick<ProgressEvent, 'total' | 'loaded'>
+import { Subject } from 'rxjs';
+export const onProgress$: Subject<WasmLoadProgressEvent>;
---
+++ src/assets/webnode/mina-rust/wasm_bg.wasm.d.ts
@@ -0,0 +1,37 @@
+/* tslint:disable */
+/* eslint-disable */
+export const memory: WebAssembly.Memory;
+export function __wbg_inmemlog_free(a: number): void;
+export function inmemlog_get(a: number, b: number, c: number, d: number): void;
+export function inmemlog_id(a: number): number;
+export function inmemlog_as_json(a: number, b: number): void;
+export function __wbg_manualconnector_free(a: number): void;
+export function manualconnector_dial(a: number, b: number, c: number): number;
+export function manualconnector_listen(a: number): number;
+export function start(): number;
+export function __wbg_jshandle_free(a: number): void;
+export function jshandle_logs_range(a: number, b: number, c: number, d: number, e: number): number;
+export function jshandle_manual_connector(a: number): number;
+export function jshandle_is_peer_id_valid(a: number, b: number, c: number, d: number): void;
+export function jshandle_global_state_get(a: number): number;
+export function jshandle_peer_connect(a: number, b: number, c: number): number;
+export function jshandle_pubsub_publish(a: number, b: number, c: number, d: number): number;
+export function jshandle_generate_account_keys(a: number): number;
+export function jshandle_payment_sign_and_inject(a: number, b: number): number;
+export function __wbg_crypto_free(a: number): void;
+export function crypto_pub_key_as_protobuf(a: number, b: number): void;
+export function crypto_peer_id_as_b58(a: number, b: number): void;
+export function crypto_sign(a: number, b: number, c: number, d: number): void;
+export function crypto_pub_key_as_protobuf_to_peer_id_as_b58(a: number, b: number, c: number, d: number): void;
+export function crypto_assert_signature(a: number, b: number, c: number, d: number, e: number, f: number, g: number, h: number): void;
+export function crypto_pub_key_as_protobuf_to_peer_id(a: number, b: number, c: number, d: number, e: number, f: number): void;
+export function __wbindgen_malloc(a: number): number;
+export function __wbindgen_realloc(a: number, b: number, c: number): number;
+export const __wbindgen_export_2: WebAssembly.Table;
+export function _dyn_core__ops__function__FnMut__A____Output___R_as_wasm_bindgen__closure__WasmClosure___describe__invoke__hf0d3ac44e8ea9df5(a: number, b: number, c: number): void;
+export function _dyn_core__ops__function__FnMut_____Output___R_as_wasm_bindgen__closure__WasmClosure___describe__invoke__h724fb25c7b600c05(a: number, b: number): void;
+export function _dyn_core__ops__function__FnMut_____Output___R_as_wasm_bindgen__closure__WasmClosure___describe__invoke__h5e2c95f786bffb19(a: number, b: number): void;
+export function __wbindgen_add_to_stack_pointer(a: number): number;
+export function __wbindgen_free(a: number, b: number): void;
+export function __wbindgen_exn_store(a: number): void;
+export function wasm_bindgen__convert__closures__invoke2_mut__h419d610678e97774(a: number, b: number, c: number, d: number): void;
