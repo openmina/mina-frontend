220c03ee8af3565b458726ef099b759d2d839224
Jolte Teofil <jolteteofil@yahoo.com>
2023-03-03 14:36:16 +0200

    Fix test

--- cypress/e2e/explorer/snark-traces/explorer-snark-traces-toolbar.cy.ts
+++ cypress/e2e/explorer/snark-traces/explorer-snark-traces-toolbar.cy.ts
@@ -79,8 +79,7 @@ describe('EXPLORER SNARK TRACES TOOLBAR', () => {
                 expect(state2.filter.workers.length).to.equal(2);
                 expect(state2.filter.workers[0]).to.equal(state2.workers[0]);
                 expect(state2.filter.workers[1]).to.equal(state2.workers[1]);
-                cy.get('.mina-table')
-                  .get('.row:not(.head) > span:nth-child(1)')
+                cy.get('.mina-table .row:not(.head) > span:nth-child(1)')
                   .then((spans: any) => {
                     Array.from(spans).forEach((span: any) => {
                       expect(span.innerText).to.be.oneOf([truncateMid(state.workers[0], 0, 12), truncateMid(state.workers[1], 0, 12)]);
