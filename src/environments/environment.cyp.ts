4b9ecc7457b59141686b8e359e06207edd27631d
Jolte Teofil <jolteteofil@yahoo.com>
2023-03-06 14:22:29 +0200

    Drone

--- src/environments/environment.cyp.ts
+++ src/environments/environment.cyp.ts
@@ -3,35 +3,52 @@ import { MinaEnv } from '@shared/types/core/environment/mina-env.type';
 export const environment: Readonly<MinaEnv> = {
   production: false,
   aggregator: '/agg',
-  configs: [{
-    backend: 'http://1.k8.openmina.com:31308/node1',
-    debugger: 'http://1.k8.openmina.com:31308/node1/bpf-debugger',
-    features: ['dashboard', 'network', 'benchmarks', 'explorer', 'tracing', 'resources', 'logs', 'web-node'],
-    name: 'node1',
-  }, {
-    backend: 'http://1.k8.openmina.com:31308/node2',
-    debugger: 'http://1.k8.openmina.com:31308/node2/bpf-debugger',
-    features: ['dashboard', 'network', 'benchmarks', 'explorer', 'tracing', 'resources', 'logs', 'web-node'],
-    name: 'node2',
-  }, {
-    backend: 'http://1.k8.openmina.com:31308/node3',
-    debugger: 'http://1.k8.openmina.com:31308/node3/bpf-debugger',
-    features: ['dashboard', 'network', 'benchmarks', 'explorer', 'tracing', 'resources', 'logs', 'web-node'],
-    name: 'node3',
-  }, {
-    backend: 'http://1.k8.openmina.com:31308/node4',
-    debugger: 'http://1.k8.openmina.com:31308/node4/bpf-debugger',
-    features: ['dashboard', 'network', 'benchmarks', 'explorer', 'tracing', 'resources', 'logs', 'web-node'],
-    name: 'node4',
-  }, {
-    backend: 'http://1.k8.openmina.com:31308/node5',
-    debugger: 'http://1.k8.openmina.com:31308/node5/bpf-debugger',
-    features: ['dashboard', 'network', 'benchmarks', 'explorer', 'tracing', 'resources', 'logs', 'web-node'],
-    name: 'node5',
-  }, {
-    backend: 'http://1.k8.openmina.com:31308/snarker9',
-    debugger: 'http://1.k8.openmina.com:31308/snarker9/bpf-debugger',
-    features: ['dashboard', 'network', 'benchmarks', 'explorer', 'tracing', 'resources', 'logs', 'web-node'],
-    name: 'snarker9',
-  }],
+  configs: [
+    {
+      backend: 'http://1.k8.openmina.com:31311/node1',
+      debugger: 'http://1.k8.openmina.com:31311/node1/bpf-debugger',
+      features: ['dashboard', 'network', 'benchmarks', 'explorer', 'tracing', 'resources', 'logs', 'web-node'],
+      name: 'node1',
+    },
+    {
+      backend: 'http://1.k8.openmina.com:31311/node2',
+      debugger: 'http://1.k8.openmina.com:31311/node2/bpf-debugger',
+      features: ['dashboard', 'network', 'benchmarks', 'explorer', 'tracing', 'resources', 'logs', 'web-node'],
+      name: 'node2',
+    },
+    {
+      backend: 'http://1.k8.openmina.com:31311/node3',
+      debugger: 'http://1.k8.openmina.com:31311/node3/bpf-debugger',
+      features: ['dashboard', 'network', 'benchmarks', 'explorer', 'tracing', 'resources', 'logs', 'web-node'],
+      name: 'node3',
+    },
+    {
+      backend: 'http://1.k8.openmina.com:31311/node4',
+      debugger: 'http://1.k8.openmina.com:31311/node4/bpf-debugger',
+      features: ['dashboard', 'network', 'benchmarks', 'explorer', 'tracing', 'resources', 'logs', 'web-node'],
+      name: 'node4',
+    },
+    {
+      backend: 'http://1.k8.openmina.com:31311/node5',
+      debugger: 'http://1.k8.openmina.com:31311/node5/bpf-debugger',
+      features: ['dashboard', 'network', 'benchmarks', 'explorer', 'tracing', 'resources', 'logs', 'web-node'],
+      name: 'node5',
+    },
+    {
+      backend: 'http://1.k8.openmina.com:31311/snarker9',
+      debugger: 'http://1.k8.openmina.com:31311/snarker9/bpf-debugger',
+      features: ['dashboard', 'network', 'benchmarks', 'explorer', 'tracing', 'resources', 'logs', 'web-node'],
+      name: 'snarker9',
+    },
+    {
+      backend: 'http://1.k8.openmina.com:31311/prod1',
+      name: 'prod1',
+      features: ['dashboard', 'network', 'benchmarks', 'explorer', 'tracing', 'resources', 'web-node'],
+    },
+    {
+      backend: 'http://1.k8.openmina.com:31311/seed1',
+      name: 'seed1',
+      features: ['dashboard', 'network', 'benchmarks', 'explorer', 'tracing', 'resources', 'logs', 'web-node'],
+    },
+  ],
 };
