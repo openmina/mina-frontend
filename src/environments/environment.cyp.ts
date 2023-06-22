1e51026d5a80763c32288118b5adaf183e0ad8f4
Jolte Teofil <jolteteofil@yahoo.com>
2023-06-22 10:26:39 +0300

    Drone Docker Build

--- src/environments/environment.cyp.ts
+++ src/environments/environment.cyp.ts
@@ -2,53 +2,57 @@ import { MinaEnv } from '@shared/types/core/environment/mina-env.type';
 
 export const environment: Readonly<MinaEnv> = {
   production: false,
-  aggregator: '/agg',
+  identifier: 'local',
+  aggregator: 'http://1.k8.openmina.com:31308/aggregator',
+  nodeLister: {
+    domain: 'http://65.21.195.80',
+    port: 4000,
+  },
+  isVanilla: true,
+  globalConfig: {
+    features: {
+      dashboard: ['nodes', 'topology'],
+      explorer: ['blocks', 'transactions', 'snark-pool', 'scan-state', 'snark-traces'],
+      resources: ['system'],
+      network: ['messages', 'connections', 'blocks', 'blocks-ipc'],
+      tracing: ['overview', 'blocks'],
+      benchmarks: ['wallets', 'transactions'],
+      storage: ['accounts'],
+      'snark-worker': ['dashboard', 'actions'],
+      'web-node': ['wallet', 'peers', 'logs', 'state'],
+    },
+  },
   configs: [
     {
-      graphql: 'http://1.k8.openmina.com:31311/node1',
-      debugger: 'http://1.k8.openmina.com:31311/node1/bpf-debugger',
-      features: ['dashboard', 'network', 'benchmarks', 'explorer', 'tracing', 'resources', 'logs', 'web-node'],
-      name: 'node1',
+      "graphql": "http://1.k8.openmina.com:31308/node1",
+      "tracing-graphql": "http://1.k8.openmina.com:31308/node1/internal-trace",
+      "debugger": "http://1.k8.openmina.com:31308/node1/bpf-debugger",
+      "name": "node1"
     },
     {
-      graphql: 'http://1.k8.openmina.com:31311/node2',
-      debugger: 'http://1.k8.openmina.com:31311/node2/bpf-debugger',
-      features: ['dashboard', 'network', 'benchmarks', 'explorer', 'tracing', 'resources', 'logs', 'web-node'],
-      name: 'node2',
+      "graphql": "http://1.k8.openmina.com:31308/node2",
+      "tracing-graphql": "http://1.k8.openmina.com:31308/node2/internal-trace",
+      "debugger": "http://1.k8.openmina.com:31308/node2/bpf-debugger",
+      "name": "node2"
     },
     {
-      graphql: 'http://1.k8.openmina.com:31311/node3',
-      debugger: 'http://1.k8.openmina.com:31311/node3/bpf-debugger',
-      features: ['dashboard', 'network', 'benchmarks', 'explorer', 'tracing', 'resources', 'logs', 'web-node'],
-      name: 'node3',
+      "graphql": "http://1.k8.openmina.com:31308/node3",
+      "tracing-graphql": "http://1.k8.openmina.com:31308/node3/internal-trace",
+      "debugger": "http://1.k8.openmina.com:31308/node3/bpf-debugger",
+      "name": "node3"
     },
     {
-      graphql: 'http://1.k8.openmina.com:31311/node4',
-      debugger: 'http://1.k8.openmina.com:31311/node4/bpf-debugger',
-      features: ['dashboard', 'network', 'benchmarks', 'explorer', 'tracing', 'resources', 'logs', 'web-node'],
-      name: 'node4',
+      "graphql": "http://1.k8.openmina.com:31308/prod01",
+      "tracing-graphql": "http://1.k8.openmina.com:31308/prod01/internal-trace",
+      "debugger": "http://1.k8.openmina.com:31308/prod01/bpf-debugger",
+      "name": "prod01"
     },
     {
-      graphql: 'http://1.k8.openmina.com:31311/node5',
-      debugger: 'http://1.k8.openmina.com:31311/node5/bpf-debugger',
-      features: ['dashboard', 'network', 'benchmarks', 'explorer', 'tracing', 'resources', 'logs', 'web-node'],
-      name: 'node5',
-    },
-    {
-      graphql: 'http://1.k8.openmina.com:31311/snarker9',
-      debugger: 'http://1.k8.openmina.com:31311/snarker9/bpf-debugger',
-      features: ['dashboard', 'network', 'benchmarks', 'explorer', 'tracing', 'resources', 'logs', 'web-node'],
-      name: 'snarker9',
-    },
-    {
-      graphql: 'http://1.k8.openmina.com:31311/prod1',
-      name: 'prod1',
-      features: ['dashboard', 'network', 'benchmarks', 'explorer', 'tracing', 'resources', 'web-node'],
-    },
-    {
-      graphql: 'http://1.k8.openmina.com:31311/seed1',
-      name: 'seed1',
-      features: ['dashboard', 'network', 'benchmarks', 'explorer', 'tracing', 'resources', 'logs', 'web-node'],
+      "graphql": "http://1.k8.openmina.com:31308/prod02",
+      "tracing-graphql": "http://1.k8.openmina.com:31308/prod02/internal-trace",
+      "debugger": "http://1.k8.openmina.com:31308/prod02/bpf-debugger",
+      "name": "prod02"
     },
   ],
 };
+
