4b482408cc3880af187c8353464a4f9dfd9a8527
Jolte Teofil <jolteteofil@yahoo.com>
2023-06-27 14:36:19 +0300

    Dashboard tests

--- src/assets/env.js
+++ src/assets/env.js
@@ -1,14 +1,26 @@
 (function (window) {
   window['env'] = {
     production: true,
-    configs: [
-      {
-        backend: 'https://debug.dev.openmina.com:3086',
-        debugger: 'https://debug.dev.openmina.com',
-        minaExplorer: 'https://berkeley.api.minaexplorer.com',
-        features: ['network', 'benchmarks'],
-        name: 'debug',
+    identifier: 'openmina',
+    aggregator: 'http://1.k8.openmina.com:31308/aggregator',
+    nodeLister: {
+      domain: 'http://65.21.195.80',
+      port: 4000,
+    },
+    isVanilla: true,
+    globalConfig: {
+      features: {
+        dashboard: ['nodes'],
+        // explorer: ['blocks', 'transactions', 'snark-pool', 'scan-state', 'snark-traces'],
+        // resources: ['system'],
+        // network: ['messages', 'connections', 'blocks', 'blocks-ipc'],
+        tracing: ['overview', 'blocks'],
+        // benchmarks: ['wallets', 'transactions'],
+        // storage: ['accounts'],
+        // 'snark-worker': ['dashboard', 'actions'],
+        // 'web-node': ['wallet', 'peers', 'logs', 'state'],
       },
-    ],
-  };
+    },
+    configs: [],
+  }
 })(this);
