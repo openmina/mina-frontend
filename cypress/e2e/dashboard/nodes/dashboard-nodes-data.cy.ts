89ea838fcfbc9236750f16800fa4c8a7119aab20
Jolte Teofil <jolteteofil@yahoo.com>
2023-06-30 14:42:02 +0300

    Dashboard nodes data checking

--- cypress/e2e/dashboard/nodes/dashboard-nodes-data.cy.ts
+++ cypress/e2e/dashboard/nodes/dashboard-nodes-data.cy.ts
@@ -3,6 +3,7 @@ import { MinaState } from '@app/app.setup';
 import { stateSliceAsPromise } from '../../../support/commands';
 import { DashboardNodesState } from '@dashboard/nodes/dashboard-nodes.state';
 import { AppNodeStatusTypes } from '@shared/types/app/app-node-status-types.enum';
+import Bluebird from 'cypress/types/bluebird';
 import AUTWindow = Cypress.AUTWindow;
 
 const condition = (state: DashboardNodesState) => state && state.nodes.length > 1;
@@ -69,7 +70,47 @@ describe('DASHBOARD NODES DATA CONSISTENCY', () => {
       nodesWithRpcError: number;
     };
     const data: NodeStatsPerPage[] = [];
-    let initialSlot: number;
+    let nodes: any[] = [];
+    let maxGlobalSlot: number = 0;
+
+    function findLatestGlobalSlot(index: number) {
+      if (index >= nodes.length) {
+        return Cypress.Promise.resolve();
+      }
+
+      return new Cypress.Promise((resolve) => {
+        cy.request('POST', nodes[index].tracingUrl, { query: `query latestBlockHeight { blockTraces(maxLength: 1, order: Descending) }` })
+          .then((response: any) => {
+            maxGlobalSlot = Math.max(maxGlobalSlot, response.body.data.blockTraces.traces[0].global_slot);
+            resolve(findLatestGlobalSlot(index + 1));
+          });
+      });
+    }
+
+    function checkNodesForGlobalSlot(index: number, globalSlot: number) {
+      if (index >= nodes.length) {
+        return Cypress.Promise.resolve();
+      }
+      return new Cypress.Promise((resolve) => {
+        cy.request({
+          method: 'POST',
+          url: nodes[index].tracingUrl,
+          body: { query: `query traces { blockTraces(global_slot: ${globalSlot}) }` },
+          failOnStatusCode: false,
+        })
+          .then((response: Response) => {
+
+            const dataIndex = data.findIndex(d => d.globalSlot === globalSlot);
+            if (response.status !== 200) {
+              data[dataIndex].nodesWithRpcError++;
+            } else if ((response.body as any).data.blockTraces.traces.length === 0) {
+              data[dataIndex].nodesMissingData++;
+            }
+
+            resolve(checkNodesForGlobalSlot(index + 1, globalSlot));
+          });
+      });
+    }
 
     cy.visit(Cypress.config().baseUrl + '/tracing')
       .wait(1000)
@@ -78,36 +119,42 @@ describe('DASHBOARD NODES DATA CONSISTENCY', () => {
         const nodeLister = (window as any).config.nodeLister;
         if (!nodeLister) return;
 
-        let nodes: any[] = [];
-        let maxGlobalSlot: number = 0;
-
         cy.request('GET', nodeLister.domain + ':' + nodeLister.port + '/nodes')
           .then((response: any) => {
-            nodes = response.body.slice(0, 10).map((node: any) => {
-              return ({
-                ...{} as any,
-                name: `${node.ip}:${node.graphql_port}`,
-                url: `${node.ip}:${node.graphql_port}/graphql`,
-                tracingUrl: `${nodeLister.domain}:${node.internal_trace_port}/graphql`,
-                status: AppNodeStatusTypes.SYNCED,
-                forks: [],
-              });
-            });
-            console.log(nodes);
-            expect(nodes).to.have.length(10);
-          });
 
-        cy.wrap(nodes).each((node: any) => {
-          return new Cypress.Promise((resolve) => {
-            console.log(node);
-            // cy.request('POST', node.url, { query: `query latestBlockHeight { blockTraces(maxLength: 1, order: Descending) }` })
-            //   .then((response: any) => {
-            //     maxGlobalSlot = Math.max(maxGlobalSlot, response.body.data.blockTraces.traces[0].global_slot);
-            //     console.log('maxGlobalSlot', maxGlobalSlot);
-            //   });
+            nodes = response.body.map((node: any) => ({
+              name: `${node.ip}:${node.graphql_port}`,
+              url: `${node.ip}:${node.graphql_port}/graphql`,
+              tracingUrl: `${nodeLister.domain}:${node.internal_trace_port}/graphql`,
+              status: AppNodeStatusTypes.SYNCED,
+            }));
+
+            let globalSlotPromises: Bluebird<any>[] = [];
+            findLatestGlobalSlot(0)
+              .then(() => {
+                for (let i = 0; i < SLOTS_TO_CHECK; i++) {
+                  const newGlobalSlot = maxGlobalSlot - i;
+                  data.push({
+                    globalSlot: newGlobalSlot,
+                    totalNodes: nodes.length,
+                    nodesWithRpcError: 0,
+                    nodesMissingData: 0,
+                  });
+                  globalSlotPromises.push(checkNodesForGlobalSlot(0, newGlobalSlot));
+                }
+
+                return cy.wrap(Promise.all(globalSlotPromises))
+                  .log(
+                    `|--------------------|
+                     |-------RESULT-------|
+                     |--------------------|`,
+                  )
+                  .log(JSON.stringify(data, null, 2));
+
+              });
           });
-        });
-      });
+      })
+      .then(() => expect(data.length).to.eq(SLOTS_TO_CHECK));
 
   });
 
