c3671bfa5a266f647b1ce7d62ba5401b7d102bfd
Jolte Teofil <jolteteofil@yahoo.com>
2023-08-02 14:12:07 +0300

    Add buttons for JSOn and improve scrolling

--- src/app/features/dsw/work-pool/dsw-work-pool-side-panel/dsw-work-pool-side-panel.component.ts
+++ src/app/features/dsw/work-pool/dsw-work-pool-side-panel/dsw-work-pool-side-panel.component.ts
@@ -1,31 +1,49 @@
-import { ChangeDetectionStrategy, Component, OnInit } from '@angular/core';
+import { ChangeDetectionStrategy, Component, OnInit, ViewChild } from '@angular/core';
 import { StoreDispatcher } from '@shared/base-classes/store-dispatcher.class';
-import { ExpandTracking } from '@shared/components/mina-json-viewer/mina-json-viewer.component';
+import { ExpandTracking, MinaJsonViewerComponent } from '@shared/components/mina-json-viewer/mina-json-viewer.component';
 import { selectDswWorkPoolActiveWorkPool } from '@dsw/work-pool/dsw-work-pool.state';
 import { WorkPool } from '@shared/types/dsw/work-pool/work-pool.type';
 import { DswWorkPoolSetActiveWorkPool } from '@dsw/work-pool/dsw-work-pool.actions';
+import { downloadJson } from '@app/shared/helpers/user-input.helper';
 
 @Component({
   selector: 'mina-dsw-work-pool-side-panel',
   templateUrl: './dsw-work-pool-side-panel.component.html',
   styleUrls: ['./dsw-work-pool-side-panel.component.scss'],
   changeDetection: ChangeDetectionStrategy.OnPush,
+  host: { class: 'flex-column h-100' },
 })
 export class DswWorkPoolSidePanelComponent extends StoreDispatcher implements OnInit {
 
   activeWorkPool: WorkPool;
   expandTracking: ExpandTracking = {};
+  jsonString: string;
+
+  @ViewChild(MinaJsonViewerComponent) private minaJsonViewer: MinaJsonViewerComponent;
 
   ngOnInit(): void {
     this.select(selectDswWorkPoolActiveWorkPool, (wp: WorkPool) => {
       this.activeWorkPool = { ...wp };
       delete this.activeWorkPool.types;
       delete this.activeWorkPool.typesSort;
+      this.jsonString = JSON.stringify(this.activeWorkPool);
       this.detect();
     });
   }
 
   closeSidePanel(): void {
     this.dispatch(DswWorkPoolSetActiveWorkPool, { id: undefined });
+  }
+
+  downloadJson(): void {
+    downloadJson(this.jsonString, 'work-pool.json');
+  }
+
+  expandEntireJSON(): void {
+    this.expandTracking = this.minaJsonViewer.toggleAll(true);
+  }
+
+  collapseEntireJSON(): void {
+    this.expandTracking = this.minaJsonViewer.toggleAll(false);
   }
 }
